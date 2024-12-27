a789ad44003b626bb46b5c7019ca3d59ba2b75bb
Bongx514 <bonganithwala514@live.com>
2024-12-27 17:42:50 +0200

    Added toggle functionality to the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.less'
 })
 export class NavbarComponent {
+  isMenuOpen = false; // Track the menu's state
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Toggle the menu state
+  }
 }
