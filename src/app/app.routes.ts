51f24a0c28a7d59aa8af1000f2d05a656223f06b
Bongx514 <bonganithwala514@live.com>
2025-02-19 20:16:38 +0200

    Add contact page, and removing inline styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { OurServicesComponent } from './pages/our-services/our-services.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'our-services', component: OurServicesComponent },
     { path: 'about-us', component: AboutUsComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
